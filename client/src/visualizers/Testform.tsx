27fe197d58ad9ccea8719b2aa72ca263e28d6375
Nathaniel Duya <nathanduya@gmail.com>
2021-11-15 09:30:49 -0800

    Two options

--- client/src/visualizers/Testform.tsx
+++ client/src/visualizers/Testform.tsx
@@ -10,17 +10,38 @@ export const Testform = new Visualizer(
   (p5: P5, analyzer: Tone.Analyser) => {
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
-    const space_between_lines = (width/2) / 64;
+    const space_between_lines = (width / 2) / 128;
+    const radius = width / 10;
 
     p5.background(0, 0, 0, 255);
 
+
+
+    p5.translate( width / 2.5, height / 2 );
+ 
+    p5.stroke( 0, 0, 255 );
+    p5.ellipse( 0, 0, radius );
+
     let values = analyzer.getValue();
-    for (let i = 0; i < values.length; i++) {
-      p5.fill(i*3,255,255); //fill(i,255,255);
-      const amp = values[i] as number;
-      const y = height + amp * height;
-      p5.rect((width/2) + (i * space_between_lines), y, space_between_lines, y);
-      p5.rect((width/2) - (i * space_between_lines), y, space_between_lines, height - y);
+    let up = 0;
+    let down = values.length;
+    while(up < values.length){
+      p5.fill(up*3,255,255);
+      const upAmp = values[up] as number;
+      const downAmp = values[down] as number;
+      const uY = height + upAmp * height;
+      const dY = height + downAmp * height;
+
+      p5.rotate( p5.TWO_PI / values.length );
+      // p5.rect(radius / 2, uY / 8, space_between_lines, height - uY);
+      // p5.rect(radius, dY / 4, space_between_lines, height - dY);
+      p5.stroke( 255, 0, 0 );
+      p5.line( 10, radius, 0, height - uY );
+    
+      p5.stroke( 0 , 0, 255 );
+      p5.line( -10, radius/2, 0, height - dY ); 
+      down--;
+      up++;
     }
   },
 );
