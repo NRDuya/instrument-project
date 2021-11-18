11356317933ffa39d15c419817a0ee7cd7fee820
Nathaniel Duya <nathanduya@gmail.com>
2021-11-18 10:48:12 -0800

    Updated names

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -4,7 +4,8 @@ import { List, Map } from 'immutable';
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
 import { WaveformVisualizer } from './visualizers/Waveform';
-import { Testform } from './visualizers/Testform'; 
+import { NRDuya } from './visualizers/NRDuya-2'; 
+import { XylophoneInstrument } from './instruments/Xylophone';
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -18,8 +19,8 @@ import { Testform } from './visualizers/Testform';
  */
 export type AppState = Map<string, any>;
 
-const instruments = List([PianoInstrument]);
-const visualizers = List([WaveformVisualizer, Testform]);
+const instruments = List([PianoInstrument, XylophoneInstrument]);
+const visualizers = List([WaveformVisualizer, NRDuya]);
 export const defaultState: AppState = Map<string, any>({
   instruments,
   visualizers,
--- client/src/visualizers/NRDuya-2.tsx
+++ client/src/visualizers/NRDuya-2.tsx
@@ -5,8 +5,8 @@ import * as Tone from 'tone';
 // project imports
 import { Visualizer } from '../Visualizers';
 
-export const Testform = new Visualizer(
-  'Testform',
+export const NRDuya = new Visualizer(
+  'NRDuya',
   (p5: P5, analyzer: Tone.Analyser) => {
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
