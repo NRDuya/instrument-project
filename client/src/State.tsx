5a4d4fc467d469e46bc54801dfc4183235108613
Switch24-7 <stephenjusto24@gmail.com>
2021-11-18 11:38:31 -0800

    Merge everyone's visualizers

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -3,14 +3,14 @@ import { List, Map } from 'immutable';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
-//import { WaveformVisualizer } from './visualizers/ahuang9-3';
+import { TwoDimensionalVisualizer } from './visualizers/ahuang9-3';
 import { WaveformVisualizer } from './visualizers/Waveform';
-import { GuitarInstrument } from './instruments/Switch24-7-2';
-import { ColorSplashVisualizer } from './visualizers/Switch24-7-2';
-import { NRDuya } from './visualizers/NRDuya-2'; 
-import { XylophoneInstrument } from './instruments/Xylophone';
-import { PianoInstrument2 } from './instruments/AnthonyJHizon-1';
-import { AnthonyVisualizer } from './visualizers/AnthonyJHizon-1';
+import { GuitarInstrument } from './instruments/Switch24-7-1';
+import { ColorSplashVisualizer } from './visualizers/Switch24-7-1';
+import { NRDuya } from './visualizers/NRDuya-2';
+//import { XylophoneInstrument } from './instruments/Xylophone';
+import { PianoInstrument2 } from './instruments/AnthonyJHizon-4';
+import { AnthonyVisualizer } from './visualizers/AnthonyJHizon-4';
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -24,8 +24,8 @@ import { AnthonyVisualizer } from './visualizers/AnthonyJHizon-1';
  */
 export type AppState = Map<string, any>;
 
-const instruments = List([PianoInstrument, PianoInstrument2, GuitarInstrument, XylophoneInstrument]);
-const visualizers = List([WaveformVisualizer, AnthonyVisualizer, ColorSplashVisualizer, NRDuya]);
+const instruments = List([PianoInstrument, PianoInstrument2, GuitarInstrument]);
+const visualizers = List([WaveformVisualizer, AnthonyVisualizer, ColorSplashVisualizer, NRDuya, TwoDimensionalVisualizer]);
 export const defaultState: AppState = Map<string, any>({
   instruments,
   visualizers,
